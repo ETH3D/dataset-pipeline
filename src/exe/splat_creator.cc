7480ca6863918bcaba1c4cf3a6488b7ae17c24d7
Clément Pinard <clempinard@gmail.com>
2020-06-23 18:50:22 +0200

    Bugfix: only set start_index at the end  - This bug occurs because by the time we decide to add the splat, the cloud size may have changed. As such, the mesh faces will not point to the right points

--- src/exe/splat_creator.cc
+++ src/exe/splat_creator.cc
@@ -53,7 +53,7 @@ void ConvertPCLMeshToIGL(
   pcl::PointCloud<pcl::PointXYZ>::Ptr mesh_vertex_cloud(
       new pcl::PointCloud<pcl::PointXYZ>());
   pcl::fromPCLPointCloud2(polygon_mesh.cloud, *mesh_vertex_cloud);
-  
+
   vertices->resize(mesh_vertex_cloud->size(), 3);
   for (std::size_t i = 0; i < mesh_vertex_cloud->size(); ++ i) {
     const pcl::PointXYZ& point = mesh_vertex_cloud->at(i);
@@ -61,7 +61,7 @@ void ConvertPCLMeshToIGL(
     (*vertices)(i, 1) = point.y;
     (*vertices)(i, 2) = point.z;
   }
-  
+
   faces->resize(polygon_mesh.polygons.size(), 3);
   for (std::size_t i = 0; i < polygon_mesh.polygons.size(); ++ i) {
     const pcl::Vertices& vertices = polygon_mesh.polygons[i];
@@ -77,7 +77,7 @@ int main(int argc, char** argv) {
   FLAGS_logtostderr = 1;
   google::InitGoogleLogging(argv[0]);
   pcl::console::setVerbosityLevel(pcl::console::L_ALWAYS);
-  
+
   // Parse arguments.
   std::string point_normal_cloud_path;
   pcl::console::parse_argument(argc, argv, "--point_normal_cloud_path", point_normal_cloud_path);
@@ -90,12 +90,12 @@ int main(int argc, char** argv) {
   float max_splat_size = std::numeric_limits<float>::infinity();
   pcl::console::parse_argument(argc, argv, "--max_plat_size", max_splat_size);
   const float squared_distance_threshold = distance_threshold * distance_threshold;
-  
+
   if (point_normal_cloud_path.empty() || mesh_path.empty() || output_path.empty()) {
     std::cout << "Please provide input / output paths." << std::endl;
     return EXIT_FAILURE;
   }
-  
+
   // Load point cloud with normals.
   LOG(INFO) << "Loading point cloud ...";
   pcl::PointCloud<pcl::PointNormal>::Ptr point_normal_cloud(
@@ -103,23 +103,23 @@ int main(int argc, char** argv) {
   if (pcl::io::loadPLYFile(point_normal_cloud_path, *point_normal_cloud) < 0) {
     return EXIT_FAILURE;
   }
-  
+
   // Load surface mesh.
   LOG(INFO) << "Loading mesh ...";
   pcl::PolygonMesh polygon_mesh;
   if (pcl::io::loadPLYFile(mesh_path, polygon_mesh) < 0) {
     return EXIT_FAILURE;
   }
-  
+
   // Convert the mesh to igl format.
   Eigen::MatrixXf mesh_vertices;
   Eigen::MatrixXi mesh_faces;
   ConvertPCLMeshToIGL(polygon_mesh, &mesh_vertices, &mesh_faces);
-  
+
   // Setup AABB for mesh.
   igl::AABB<Eigen::MatrixXf, 3> mesh_aabb_tree;
   mesh_aabb_tree.init(mesh_vertices, mesh_faces);
-  
+
   // Setup search tree for point-normal cloud.
   constexpr int kNearestNeighborCount = 4;
   pcl::search::KdTree<pcl::PointNormal>::Ptr search_tree(
@@ -128,7 +128,7 @@ int main(int argc, char** argv) {
   search_tree->setInputCloud(point_normal_cloud);
   std::vector<int> indices(kNearestNeighborCount + 1);
   std::vector<float> squared_distances(kNearestNeighborCount + 1);
-  
+
   // Loop over all points and generate normal-aligned, variable-size splats for
   // all points which are not sufficiently well represented by the mesh.
   LOG(INFO) << "Generating splats ...";
@@ -155,20 +155,18 @@ int main(int argc, char** argv) {
     if (std::isnan(point_normal.normal_x) || std::isnan(point_normal.normal_y) || std::isnan(point_normal.normal_z)) {
       continue;
     }
-    
+
     // Determine the extents of the possible splat for this point.
     // Determine splat size as the maximum distance of the k nearest neighbors.
     CHECK_EQ(search_tree->nearestKSearch(
         point_normal, kNearestNeighborCount + 1, indices, squared_distances),
         kNearestNeighborCount + 1);
     float splat_radius = std::min(sqrtf(squared_distances[kNearestNeighborCount]), max_splat_size);
-    
+
     // Find (random) right and up vectors corresponding to the normal.
     Eigen::Vector3f right = point_normal.getNormalVector3fMap().unitOrthogonal();
     Eigen::Vector3f up = point_normal.getNormalVector3fMap().cross(right);
-    
-    // Determine the vertex positions.
-    std::size_t start_index = output_cloud->size();
+
     pcl::PointXYZ point_top_right;
     point_top_right.getVector3fMap() = point_normal.getVector3fMap() + splat_radius * (right + up);
     pcl::PointXYZ point_bottom_right;
@@ -177,7 +175,7 @@ int main(int argc, char** argv) {
     point_bottom_left.getVector3fMap() = point_normal.getVector3fMap() + splat_radius * (-right - up);
     pcl::PointXYZ point_top_left;
     point_top_left.getVector3fMap() = point_normal.getVector3fMap() + splat_radius * (-right + up);
-    
+
     // Add the splat only if the center or at least one of the vertices are not well represented in the mesh.
     bool add_splat = false;
     pcl::PointXYZ point_center;
@@ -199,11 +197,13 @@ int main(int argc, char** argv) {
     if (!add_splat && mesh_aabb_tree.squared_distance(mesh_vertices, mesh_faces, point_top_left.getVector3fMap().transpose(), closest_facet, closest_point) > squared_distance_threshold) {
       add_splat = true;
     }
-    
+  
     // Add the splat to the mesh?
     if (add_splat) {
       #pragma omp critical
       {
+        // Determine the vertex positions.
+        std::size_t start_index = output_cloud->size();
         output_cloud->push_back(point_top_right);
         output_cloud->push_back(point_bottom_right);
         output_cloud->push_back(point_bottom_left);
@@ -225,11 +225,11 @@ int main(int argc, char** argv) {
   }
   
   LOG(INFO) << "Added " << added_splat_count << " splats.";
-  
+
   // Write result mesh containing the splats.
   pcl::toPCLPointCloud2(*output_cloud, output_mesh.cloud);
   pcl::io::savePLYFileBinary(output_path, output_mesh);
-  
+
   std::cout << "Finished!" << std::endl;
   return EXIT_SUCCESS;
 }
